1d1306957958b562b711463c69ed99261e11f255
PyPranav <59741576+PyPranav@users.noreply.github.com>
2020-06-24 04:02:56 +0530

    Create News_UI.py

---
+++ Apps/newsapi-python/News_UI.py
@@ -0,0 +1,116 @@
+import tkinter as tk
+from tkinter import ttk
+from urllib.request import urlopen
+from newsapi import NewsApiClient
+from PIL import Image, ImageTk
+import webbrowser
+import io
+import urllib
+
+prev_step = 'next'
+
+
+def back(_event=None):
+    global cur, prev_step
+    if cur != 0:
+        cur -= 1
+        prev_step = 'back'
+        update()
+
+
+def next(_event=None):
+    global cur, prev_step
+    cur += 1
+    prev_step = 'next'
+    update()
+
+
+def update():
+    global title, img, description, original_site, cur
+    title.config(text='Updating..........')
+    image = ImageTk.PhotoImage(Image.open(r'res\loading_img.png'))
+    img.configure(image=image)
+    img.image = image
+    description.config(text='')
+    root.update()
+    while True:
+        try:
+            #req = urllib.request.Request(articles[cur]['urlToImage'], headers=hdr)
+            raw_data = urlopen(articles[cur]['urlToImage'], timeout=5).read()
+            im = Image.open(io.BytesIO(raw_data))
+            tk.Label(root).grid(row=1, column=0, columnspan=2)
+            image = ImageTk.PhotoImage(im)
+            break
+        except urllib.error.HTTPError:
+            if prev_step == 'next':
+                cur += 1
+            elif prev_step == 'back':
+                cur -= 1
+        except IndexError:
+            image = ImageTk.PhotoImage(Image.open(r'res\end.jpg'))
+            break
+        except Exception:
+            if prev_step == 'next':
+                cur += 1
+                print(cur)
+            elif prev_step == 'back':
+                cur -= 1
+    img.configure(image=image)
+    img.image = image
+    try:
+        title.config(text=articles[cur]["title"])
+        description.config(text=articles[cur]["description"])
+    except IndexError:
+        title.config(text='Out of news articles come back later')
+
+
+newsapi = NewsApiClient(api_key='18d4d239957f4ed9b5dd8ecb8308dd09')
+
+top_headlines = newsapi.get_top_headlines(sources='google-news-in, the-times-of-india ,bbc-news, the-verge')
+articles = top_headlines['articles']
+cur = 0
+root = tk.Tk()
+root.title('NEWS | Top Headlines')
+root.wm_iconbitmap(r'res\icon.ico')
+title = ttk.Label(root, text=articles[cur]['title'], font=('Helvetica', 20, 'bold'), wraplengt=720, justify='center')
+title.grid(row=0, column=0, columnspan=2)
+
+# hdr = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+#      Chrome/83.0.4103.106 Safari/537.36'
+#     , 'Content-Length': '136963'}
+while True:
+    try:
+        #req = urllib.request.Request(articles[cur]['urlToImage'], headers=hdr)
+        raw_data = urlopen(articles[cur]['urlToImage']).read()
+        im = Image.open(io.BytesIO(raw_data))
+        tk.Label(root).grid(row=1, column=0, columnspan=2)
+        image = ImageTk.PhotoImage(im)
+        break
+    except Exception:
+        if prev_step == 'next':
+            cur += 1
+            print(cur)
+        elif prev_step == 'back':
+            cur -= 1
+
+img = tk.Label(root, image=image, width=720, height=480)
+img.grid(row=2, column=0, columnspan=2)
+
+tk.Label(root).grid(row=3, column=0, columnspan=2)
+
+description = ttk.Label(root, text=articles[cur]['description'], font=30, wraplengt=720, justify='center')
+description.grid(row=4, column=0, columnspan=2)
+
+original_site = ttk.Button(root, text='Read More', command=lambda: webbrowser.open(articles[cur]['url'], new=2))
+original_site.grid(row=5, column=0, columnspan=2)
+
+back_btn = ttk.Button(root, text='Back', command=back)
+next_btn = ttk.Button(root, text='Next', command=next)
+back_btn.grid(row=6, column=0)
+next_btn.grid(row=6, column=1)
+ttk.Label(root).grid(row=7, column=0, columnspan=2)
+root.bind('<Right>', next)
+root.bind('<Left>', back)
+
+root.mainloop()
